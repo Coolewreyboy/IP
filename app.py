6133a59a96f1cfb6770784709dfd032996031c1a
Coolewreyboy <bukind28@gmail.com>
2025-01-23 00:50:32 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ import random
 from flask import *
 from sqlite3 import *
 from random import *
-
+from json import *
 
 app = Flask(__name__)
 app.secret_key = 'f8hwt6be'
+
+base_stat = [(0, 0)] * 223
+
 
 def list_ans(s):
     a = 'ёеуэоаыяию'
@@ -19,6 +22,31 @@ def list_ans(s):
     return result
 
 
+def check_username(username):
+    s = "1234567890qwertyuioplkjhgfdsazxcvbnm_QWERTYUIOPLKJHGFDSAZXCVBNM"
+    for i in username:
+        if i not in s:
+            return False
+    return True and 30 >= len(username) >= 5
+
+
+def check_password(password):
+    s = "012345qwertyuiopasdfghjQWERTYUIOPASDFGHJKLZXCVBNMklzxcvbnm6789"
+    for i in password:
+        if i not in s:
+            return False
+    return 30 >= len(password) >= 8
+
+
+def password_hash(password):
+    h = 0
+    x = 12345
+    mod = 10 ** 9 + 21
+    for i in password:
+        h = (h * x + ord(i)) % mod
+    return h
+
+
 def clear_coockies():
     session['check_task'] = 0
     session['word_num'] = 0
@@ -27,17 +55,35 @@ def clear_coockies():
     session['answer'] = [('', '')] * 10
 
 
-@app.route('/')
+def words(username):
+    conn = connect('data_base.db')
+    cursor = conn.cursor()
+    result = loads(cursor.execute("""SELECT statistika FROM Users WHERE Username = ?""", (username,)).fetchone()[0])
+    for i in range(len(result)):
+        a, b = result[i]
+        result[i] = (a + randint(-3, 3), b + randint(-3, 3), i)
+    result.sort()
+    l = []
+    for i in range(10):
+        l.append((cursor.execute("""SELECT word FROM words_task_1 WHERE ID = ?""", (result[i][2] + 1,)).fetchone()[
+                      0].replace('Ё', 'Е'), result[i][2]))
+    conn.close()
+    return l
 
+
+@app.route('/')
 @app.route('/login', methods=['GET'])
 def login():
     if request.method == 'GET':
         username = request.args.get('username')
         password = request.args.get('password')
 
-        conn = connect('Individual_project.db')
+        conn = connect('data_base.db')
         cursor = conn.cursor()
-        result = cursor.execute("""SELECT * FROM Users WHERE Username = ? AND Password = ?""", (username, password)).fetchone()
+        if not username or not password:
+            return render_template('login.html', message='')
+        result = cursor.execute("""SELECT * FROM Users WHERE Username = ? AND Password = ?""",
+                                (username, password_hash(password))).fetchone()
         check_username = bool(cursor.execute("""SELECT * FROM Users WHERE Username = ?""", (username,)).fetchone())
         conn.commit()
         conn.close()
@@ -60,14 +106,23 @@ def register():
         password = request.args.get('password')
         password_again = request.args.get('password_again')
 
-        conn = connect('Individual_project.db')
+        if username and not check_username(username):
+            return render_template('register.html',
+                                   message='Логин может содержать только символы A-Z, a-z, 0-9, "_" и иметь длину от 7 до 30')
+
+        if password and not check_password(password):
+            return render_template('register.html',
+                                   message='Пароль должен иметь длину больше 7 символов и содержать символы A-Z, a-z, 0-9 и иметь длину от 7 до 30')
+
+        conn = connect('data_base.db')
         cursor = conn.cursor()
         result = cursor.execute("""SELECT * FROM Users WHERE Username = ?""", (username,)).fetchall()
 
         if len(result) == 0 and username:
             if password == password_again and username:
-                cursor.execute("""INSERT INTO Users(Username, Password, all_task1, accept_task1) VALUES(?, ?, ?, ?)""",
-                               (username, password, 0, 0))
+                cursor.execute(
+                    """INSERT INTO Users(Username, Password, all_task1, accept_task1, statistika) VALUES(?, ?, ?, ?, ?)""",
+                    (username, password_hash(password), 0, 0, dumps(base_stat)))
                 conn.commit()
                 conn.close()
                 return redirect(url_for('login'))
@@ -83,22 +138,16 @@ def task1():
     if not session['username']:
         return redirect(url_for('login'))
     if not session['check_task']:
-        conn = connect('Individual_project.db')
-        cursor = conn.cursor()
-        l = set()
-        while len(l) != 10:
-            l.add(cursor.execute("""SELECT word FROM words_task_1 WHERE ID = ?""", (randint(0, 222),)).fetchone()[0].replace('Ё', 'Е'))
-        conn.close()
-        l = list(l)
+        l = words(session['username'])
         session['check_task'] = 1
         session['word_num'] = 0
         session['score'] = 0
         session['list'] = l
-        session['list_ans'] = list_ans(l[0])
+        session['list_ans'] = list_ans(l[0][0])
         return render_template('task1.html')
     elif request.method == 'GET' and request.args.get('answer'):
         answer = request.args.get('answer')
-        word = session['list'][session['word_num']]
+        word = session['list'][session['word_num']][0]
         if answer == word:
             session['score'] += 1
         else:
@@ -106,8 +155,8 @@ def task1():
         session['answer'][session['word_num']] = (answer, word)
         session['word_num'] += 1
         if session['word_num'] < 10:
-            session['list_ans'] = list_ans(session['list'][session['word_num']])
-            return render_template('task1.html')
+            session['list_ans'] = list_ans(session['list'][session['word_num']][0])
+            return render_template('task1.html', score=session['score'])
         else:
             return redirect(url_for('result'))
     return render_template('task1.html')
@@ -118,14 +167,27 @@ def result():
     if not session['username']:
         return redirect(url_for('login'))
     username = session['username']
-    conn = connect('Individual_project.db')
+    conn = connect('data_base.db')
     cursor = conn.cursor()
     result = cursor.execute("""SELECT accept_task1, all_task1 FROM Users WHERE Username = ?""", (username,)).fetchone()
     accept_task1 = result[0]
     all_task1 = result[1]
     accept_task1 = accept_task1 + session['score']
     all_task1 += 10
-    result = cursor.execute("""UPDATE Users SET accept_task1 = ?, all_task1 = ? WHERE Username = ?""", (accept_task1, all_task1, username)).fetchone()
+    result = cursor.execute("""UPDATE Users SET accept_task1 = ?, all_task1 = ? WHERE Username = ?""",
+                            (accept_task1, all_task1, username)).fetchone()
+    result = loads(cursor.execute("""SELECT statistika FROM Users WHERE Username = ?""", (username,)).fetchone()[0])
+    for i in range(10):
+        s, it = session['list'][i]
+        a, b = result[it]
+        if session['status'][i] == 1:
+            result[it] = (a + 1, b - 10)
+        else:
+            result[it] = (a + 1, b + 7)
+
+    result = cursor.execute("""UPDATE Users SET statistika = ? WHERE Username = ?""",
+                            (dumps(result), username)).fetchone()
+
     conn.commit()
     conn.close()
     score = session['score']
@@ -133,6 +195,7 @@ def result():
     answer = session['answer']
     clear_coockies()
     return render_template('result.html', score=score, status=status, answer=answer)
+
 
 @app.route('/home')
 def home():
@@ -149,13 +212,14 @@ def logout():
     session['username'] = None
     return redirect(url_for('login'))
 
+
 @app.route('/profile')
 def profile():
     if not session['username']:
         return redirect(url_for('login'))
-    conn = connect("Individual_project.db")
+    conn = connect("data_base.db")
     username = session['username']
-    conn = connect('Individual_project.db')
+    conn = connect('data_base.db')
     cursor = conn.cursor()
     result = cursor.execute("""SELECT accept_task1, all_task1 FROM Users WHERE Username = ?""", (username,)).fetchone()
     accept_task1 = result[0]
@@ -164,24 +228,27 @@ def profile():
     conn.close()
     return render_template('profile.html', score=int(accept_task1 * 100 / max(all_task1, 1)))
 
+
 @app.route('/teoriy')
 def teoriy():
     if not session['username']:
         return redirect(url_for('login'))
     return render_template('teoriy.html')
 
+
 @app.route('/trophy')
 def trophy():
     if not session['username']:
         return redirect(url_for('login'))
-    conn = connect("Individual_project.db")
+    conn = connect("data_base.db")
     cursor = conn.cursor()
     result = cursor.execute("""SELECT accept_task1, all_task1, Username FROM Users """).fetchall()
     a = []
     for (ac, al, us) in result:
-        a.append((-int(ac * 100/ max(1, al)), us))
+        a.append((-int(ac * 100 / max(1, al)), us))
     a.sort()
     return render_template('trophy.html', rat=a, size=len(a))
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
